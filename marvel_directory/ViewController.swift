cce31e7b97ce4c013b1fe7eb7520c867dadbffb7
clydies freeman <cedwa@clydiess-mbp.lan>
2020-08-01 00:50:44 -0400

    added initial UI for search screen

---
+++ marvel_directory/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  marvel_directory
+//
+//  Created by clydies freeman on 7/31/20.
+//  Copyright © 2020 clydies freeman. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var browseButton: UIButton!
+    
+    private enum Constants {
+        static let cornerRadius: CGFloat = 20
+        static let errorMessageTitle = "No Query String"
+        static let errorMessageBody = "Please type atleast one character to trigger a proper search"
+        static let dismissString = "Dismiss"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        searchBar.layer.cornerRadius = Constants.cornerRadius
+        browseButton.layer.cornerRadius = Constants.cornerRadius
+        
+        hideKeyboardWhenTappedAround()
+    }
+
+
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchBarText = searchBar.text else { return }
+        
+        if searchBarText.isEmpty {
+            let alert = UIAlertController(title: Constants.errorMessageTitle, message: Constants.errorMessageBody, preferredStyle: .alert)
+            alert.addAction(.init(title: Constants.dismissString, style: .cancel))
+            present(alert, animated: true)
+        } else {
+            print("navigate to the next controller")
+        }
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
